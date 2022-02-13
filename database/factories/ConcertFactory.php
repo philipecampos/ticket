261a71a944294ca4e318b1400aa9f5f94ca5bb07
Philipe Barra <philipebarra@gmail.com>
2022-02-12 22:29:33 -0300

    Aula 3

---
+++ database/factories/ConcertFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Concert>
+ */
+class ConcertFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => 'Example Band',
+            'subtitle' => 'with The Fake Openers',
+            'date' => \Carbon\Carbon::parse('+2 weeks'),
+            'ticket_price' => 2000,
+            'venue' => 'The Example Theatre',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Fakeville',
+            'state' => 'ON',
+            'zip' => '90210',
+            'additional_information' => 'Some sample additional information.',
+        ];
+    }
+}
